d7d38b593e7caf714d77ba4d81345bd5c6b21c89
ynyanchoka <monaryymelda@gmail.com>
2022-05-17 02:35:49 +0300

    add html to about.hbs

--- src/main/java/com/group/Main.java
+++ src/main/java/com/group/Main.java
@@ -1,7 +1,18 @@
 package com.group;
 
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.SparkBase.staticFileLocation;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        staticFileLocation("/public");
     }
+//    get("/about", (request, response) -> {
+//        Map<String, Object> model = new HashMap<String, Object>();
+//        return new ModelAndView(new HashMap(), "about.hbs");
+//    }, new HandlebarsTemplateEngine());
 }
