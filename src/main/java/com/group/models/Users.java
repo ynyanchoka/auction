c0060d192f98476460ded332235c9c3dffa0faef
bryanbill <emobrya@gmail.com>
2022-05-17 12:20:24 +0300

    ft:Model - Creates user model

---
+++ src/main/java/com/group/models/Users.java
@@ -0,0 +1,36 @@
+package com.group.models;
+
+public class Users {
+
+
+    private int id;
+    private String email;
+    private String fullName;
+    private String password;
+
+    Users(String email,String fullName, String password){
+        this.email = email;
+        this.fullName = fullName;
+        this.password= password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
