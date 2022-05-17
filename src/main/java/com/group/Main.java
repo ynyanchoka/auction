f600c6134dd5bef8a94162c9cb2719c14da4b726
ynyanchoka <monaryymelda@gmail.com>
2022-05-17 12:12:51 +0300

    add changes

--- src/main/java/com/group/Main.java
+++ src/main/java/com/group/Main.java
@@ -1,18 +1,28 @@
 package com.group;
 
 import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static spark.Spark.get;
 import static spark.SparkBase.staticFileLocation;
 
 public class Main {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        //about
+        get("/about", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(new HashMap(), "about.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //item page
+        get("/item", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(new HashMap(), "about.hbs");
+        }, new HandlebarsTemplateEngine());
     }
-//    get("/about", (request, response) -> {
-//        Map<String, Object> model = new HashMap<String, Object>();
-//        return new ModelAndView(new HashMap(), "about.hbs");
-//    }, new HandlebarsTemplateEngine());
+
 }
