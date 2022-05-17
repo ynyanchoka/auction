3caef07f21bd50a445854e676207e012f93786b1
violetauko <aukoviolet24@gmail.com>
2022-05-17 14:55:55 +0300

    shopFront ui

--- src/main/java/com/group/Main.java
+++ src/main/java/com/group/Main.java
@@ -1,7 +1,21 @@
 package com.group;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"shopFront.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
