d21021eda9d1196f8e4dc36161e7779cb9090f94
ynyanchoka <monaryymelda@gmail.com>
2022-05-17 12:40:39 +0300

    add style to about.hbs

--- src/main/java/com/group/Main.java
+++ src/main/java/com/group/Main.java
@@ -1,18 +1,23 @@
 package com.group;
 
 import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static spark.Spark.get;
+import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
 public class Main {
     public static void main(String[] args) {
+        port(8080);
         staticFileLocation("/public");
+        get("/about", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(new HashMap(), "about.hbs");
+        }, new HandlebarsTemplateEngine());
     }
-//    get("/about", (request, response) -> {
-//        Map<String, Object> model = new HashMap<String, Object>();
-//        return new ModelAndView(new HashMap(), "about.hbs");
-//    }, new HandlebarsTemplateEngine());
+
 }
