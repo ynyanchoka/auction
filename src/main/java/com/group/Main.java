ae5e0c1a6faa1bcda4505934b6caba6e5fbe0e02
ynyanchoka <monaryymelda@gmail.com>
2022-05-17 13:04:00 +0300

    add color to buttons

--- src/main/java/com/group/Main.java
+++ src/main/java/com/group/Main.java
@@ -12,16 +12,11 @@ import static spark.SparkBase.staticFileLocation;
 public class Main {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        //about
-        get("/about", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(new HashMap(), "about.hbs");
-        }, new HandlebarsTemplateEngine());
 
         //item page
         get("/item", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(new HashMap(), "about.hbs");
+            return new ModelAndView(new HashMap(), "item.hbs");
         }, new HandlebarsTemplateEngine());
     }
 
